1597cf7c2a359a30678865061765cca46b348e38
Cristian Cordero <cristian.cordero@protonmail.com>
2024-06-26 15:08:32 -0400

    init commit
    init commit

---
+++ dora_report/dora_report.py
@@ -0,0 +1,152 @@
+from ipfabric import IPFClient
+from ipfabric.diagrams import Unicast
+import tomllib
+from ipfabric.tools import Vulnerabilities
+from ipfabric.tools import DeviceConfigs
+import pandas as pd
+import streamlit as st
+
+
+class FetchDataForReporting:
+    def __init__(self, toml_file_path=None, ipf_token=None, ipf_url=None, ipf_ss=None, nist_api_key=None):
+        self.toml_file_path = 'inputs/inputs.toml'
+        if toml_file_path:
+            self.toml_file_path = toml_file_path
+        if 'getvalue' in dir(self.toml_file_path):
+            self.toml_data = tomllib.loads(self.toml_file_path.getvalue().decode())
+        else:
+            with open(self.toml_file_path, 'rb') as fp:
+                self.toml_data = tomllib.load(fp)
+        ipf_snapshot = ipf_ss or self.toml_data['ipf_snapshot']
+        self.nist_api_key = nist_api_key or self.toml_data['nist_api_key']
+        self.ipf = IPFClient(
+            snapshot_id=ipf_snapshot,
+            auth=ipf_token or self.toml_data['ipf_token'],
+            base_url=ipf_url or self.toml_data['ipf_url']
+        )
+        self.ipf_columns_to_fetch = ['sn', 'version', 'hostname', 'model', 'vendor', 'siteName']
+
+    def fetch_intent_checks_data(self):
+        list_of_intent_dfs = list()
+        for get_intent_conf in self.toml_data['intent_checks']:
+            for intent_name, table_config in get_intent_conf.items():
+                if isinstance(table_config, dict):
+                    df = self.ipf.fetch_all(
+                        url=table_config['table_endpoint'],
+                        filters=table_config['filter'],
+                        reports=True,
+                        export='df',
+                    )
+                    list_of_intent_dfs.append({intent_name: df})
+        return list_of_intent_dfs
+
+    def fetch_other_check_data(self):
+        for get_intent_conf in self.toml_data['intent_checks']:
+            for intent_name, table_config in get_intent_conf.items():
+                if isinstance(table_config, bool):
+                    if intent_name == 'cve':
+                        vuln = Vulnerabilities(self.ipf, nvd_api_key=self.nist_api_key)
+                        vulns = vuln.check_versions()
+                    if intent_name == 'backups':
+                        device_config = DeviceConfigs(client=self.ipf)
+                        device_configs = device_config.get_all_configurations()
+        return vulns, device_configs
+
+    def fetch_app_data(self, for_streamlit=False):
+        return_data = list()
+        for idx, application_config in enumerate(self.toml_data['applications'], start=1):
+            app_unicast_lookup = Unicast(
+                startingPoint=application_config['source'],
+                destinationPoint=application_config['destination'],
+                dstPorts=application_config['port'],
+                protocol=application_config['protocol'],
+            )
+            app_graph_png = self.ipf.diagram.png(app_unicast_lookup)
+            app_name = application_config['name']
+            if not for_streamlit:
+                with open(f'outputs/{app_name}.png', 'wb') as f:
+                    f.write(app_graph_png)
+            else:
+                st.write(f"# Application Data {idx}")
+                st.image(app_graph_png)
+            app_graph_json = self.ipf.diagram.json(app_unicast_lookup)
+            app_graph_nodes = app_graph_json['graphResult']['graphData']['nodes']
+            device_sn_to_fetch = [
+                node_value['sn'] for node_value in app_graph_nodes.values()
+                if node_value['type'] in ['l3switch', 'fw', 'switch', 'router', 'lb']
+                ]
+            filters = {"or": [{"sn": ["like", sn]} for sn in device_sn_to_fetch]}
+            device_inventory_df = self.ipf.fetch_all(
+                url='inventory/devices',
+                filters=filters,
+                reports=True,
+                export='df',
+                columns=self.ipf_columns_to_fetch
+            )
+            return_data.append((device_inventory_df, app_graph_png))
+        return return_data
+
+    def build_report(self, all_intent_violations_list_df, vuls, device_backup, list_app_tuple, for_streamlit=False):
+        final_report_df = list()
+        all_devices = self.ipf.fetch_all(url='inventory/devices', export='df', columns=self.ipf_columns_to_fetch)
+        # process intent checks
+        all_merged_df = list()
+        for intent_response in all_intent_violations_list_df:
+            for intent_name, intent_df in intent_response.items():
+                if intent_name == 'unmanged_neighbors' or intent_df.empty:
+                    merged_df = None
+                    all_merged_df.append({intent_name: merged_df})
+                    all_devices[intent_name] = False
+                    continue
+                all_devices[intent_name] = False
+                merged_df = pd.merge(all_devices, intent_df[['sn']], on='sn')
+                all_merged_df.append({intent_name: merged_df})
+                all_devices.loc[all_devices['sn'].isin(merged_df['sn']), intent_name] = True
+
+        # process app data
+        for app_tuple in list_app_tuple:
+            app_df, app_png = app_tuple
+            for merged_df_dict in all_merged_df:
+                for intent_name, intent_df in merged_df_dict.items():
+                    if intent_df is None:
+                        app_df[intent_name] = False
+                        continue
+                    app_df[intent_name] = False
+                    merged_df = pd.merge(app_df, intent_df[['sn']], on='sn')
+                    app_df.loc[app_df['sn'].isin(merged_df['sn']), intent_name] = True
+            final_report_df.append(app_df)
+
+        # process cves
+        cve_dataframes = list()
+        all_devices['cve'] = False
+        for vul in vuls:
+            cve_totals_df = pd.DataFrame().from_dict(vul.dict()).loc['total_results']
+            cve_dataframes.append(cve_totals_df)
+        for cve_dataframe in cve_dataframes:
+            all_devices.loc[all_devices['version'].isin([cve_dataframe['version']]), 'cve'] = cve_dataframe['cves']
+            for df in final_report_df:
+                df.loc[df['version'].isin([cve_dataframe['version']]), 'cve'] = cve_dataframe['cves']
+
+        # process device backup
+        all_devices['backups'] = False
+        flattened_configs = [config for configs in device_backup.values() for config in configs]
+        backup_configs_df = pd.DataFrame([config.dict() for config in flattened_configs])
+        all_devices['backups'] = all_devices['sn'].isin(backup_configs_df['sn'])
+        for df in final_report_df:
+            df['backups'] = df['sn'].isin(backup_configs_df['sn'])
+        if for_streamlit:
+            return all_devices, final_report_df
+        all_devices.to_csv('outputs/all_devices.csv')
+        for idx, df in enumerate(final_report_df):
+            df.to_csv(f'outputs/{idx}.csv')
+        return all_devices, final_report_df
+
+
+def main():
+    fetch_data = FetchDataForReporting()
+    all_intent_violations_list_df = fetch_data.fetch_intent_checks_data()
+    vuls, device_backup = fetch_data.fetch_other_check_data()
+    list_app_tuple = fetch_data.fetch_app_data()
+    return fetch_data.build_report(all_intent_violations_list_df, vuls, device_backup, list_app_tuple)
+
+
---
+++ dora_report/dora_report_streamlit.py
@@ -0,0 +1,60 @@
+from dora_report import FetchDataForReporting
+import streamlit as st
+
+
+def main(toml_file=None, ipf_token=None, ipf_url=None, ipf_ss=None, nist_api_key=None):
+    fetch_data = FetchDataForReporting(toml_file_path=toml_file, ipf_token=ipf_token, ipf_url=ipf_url, ipf_ss=ipf_ss, nist_api_key=nist_api_key)
+    all_intent_violations_list_df = fetch_data.fetch_intent_checks_data()
+    vuls, device_backup = fetch_data.fetch_other_check_data()
+    list_app_tuple = fetch_data.fetch_app_data(for_streamlit=True)
+    return fetch_data.build_report(
+        all_intent_violations_list_df,
+        vuls,
+        device_backup,
+        list_app_tuple,
+        for_streamlit=True
+    )
+
+
+uploaded_toml = st.sidebar.file_uploader(label='inputs.toml', help='Upload the inputs.toml file')
+ipf_token = st.sidebar.text_input(label='IPFabric Token', help='Enter your IPFabric Token')
+ipf_url = st.sidebar.text_input(label='IPFabric URL', help='Enter your IPFabric URL')
+ipf_ss = st.sidebar.text_input(label='IPFabric Snapshot', help='Enter your IPFabric Snapshot UUID')
+nist_api_key = st.sidebar.text_input(label='NIST API Key', help='Enter your NIST API Key')
+try:
+    all_devs, final_report_df = main(toml_file=uploaded_toml, ipf_token=ipf_token, ipf_url=ipf_url, ipf_ss=ipf_ss, nist_api_key=nist_api_key)
+    st.write("# App Data 1")
+    st.dataframe(final_report_df[0], width=5000)
+    st.write("# App Data 2")
+    st.dataframe(final_report_df[1])
+    st.write("# All Devices")
+    st.dataframe(all_devs)
+except Exception as e:
+    st.write("Upload a valid toml file, heres and example of a valid toml file:")
+    st.write(
+        """
+        ```
+        applications = [
+    {"name" = "Internal-app", "source" = "172.16.12.60/31", "destination" = "172.16.31.60", "protocol" = "tcp", "port" = "443", "URL" = "https://app.internal/", "Comments" = "Internal app for use by users inside network"},
+    {"name" = "External-app", "source" = "172.16.21.0/24", "destination" = "172.16.32.60", "protocol" = "tcp", "port" = "443", "URL" = "https://app.external.com/", "Comments" = "External-facing app for use by users coming in over VPN"}
+]
+# Define the checks from ipfabric to preform and the requirements to fetch the data
+# Python SDK Accepts both API and Frontend endpoints
+intent_checks =[
+    {'unmanged_neighbors' = {'table_endpoint' = 'technology/cdp-lldp/unmanaged-neighbors', 'filter' = ''}},
+    {'hardware_eol' = {'table_endpoint' = 'tables/reports/eof/summary', 'filter' = '{"endSupport": ["color", "eq", "30"]}'}},
+    {'aaa'= {'table_endpoint' = 'tables/security/aaa/authorization', 'filter' = '{"primaryMethod": ["color", "eq", "20"]}'}},
+    {'ntp' = {'table_endpoint' = '/tables/management/ntp/summary', 'filter' = '{"confSources": ["color", "eq", "20"], "reachableSources": ["color", "eq", "20"]}'}},
+    {'snmp_summary' = {'table_endpoint' = '/technology/management/snmp/summary', 'filter' = '{"or": [{"communitiesCount": ["color", "eq", "20"]}, {"communitiesCount": ["color", "eq", "10"]}]}'}},
+    {'snmp_communities' = {'table_endpoint' = '/tables/management/snmp/communities', 'filter' = '{"name": ["color", "eq", "20"]}'}},
+    {'management_access' = {'table_endpoint' = 'tables/security/aaa/lines', 'filter' = '{"inTransports": ["any", "like", "telnet"]}'}},
+    {'cve' = true},
+    {'backups' = true}
+]
+
+ipf_snapshot = '40e3426f-877e-46ee-a48d-18868b51262d'
+```
+        """
+    )
+    st.write(f"Error: {e}")
+
